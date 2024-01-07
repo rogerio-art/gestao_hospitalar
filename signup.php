98654393c345d6d45e034aaf5f43226102c889bd
rogerio-art <rogeriolameira2017@gmail.com>
2024-01-07 20:53:17 +0000

    update20240107

--- signup.php
+++ signup.php
@@ -1,6 +1,6 @@
 <?php include('headerlogout.php');?>
 
-<link rel="favicon" href="assets/frontend/default/img/icons/favicon.ico' ?>">
+<link rel="favicon" href="assets/frontend/default/img/icons/favicon.ico">
 <link rel="apple-touch-icon" href="assets/frontend/default/img/icons/icon.png">
 <link rel="stylesheet" href="assets/frontend/default/css/jquery.webui-popover.min.css">
 <link rel="stylesheet" href="assets/frontend/default/css/select2.min.css">
@@ -16,7 +16,25 @@
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700" rel="stylesheet">
 <link rel="stylesheet" href="assets/global/toastr/toastr.css' ?>">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nestable2/1.6.0/jquery.nestable.min.css" />
-<script src="('assets/backend/js/jquery-3.3.1.min.js"></script>
+<script src="assets/backend/js/jquery-3.3.1.min.js"></script>
+
+<script src="assets/frontend/default/js/vendor/modernizr-3.5.0.min.js"></script>
+<script src="assets/frontend/default/js/vendor/jquery-3.2.1.min.js"></script>
+<script src="assets/frontend/default/js/popper.min.js"></script>
+<script src="assets/frontend/default/js/bootstrap.min.js"></script>
+<script src="assets/frontend/default/js/slick.min.js"></script>
+<script src="assets/frontend/default/js/select2.min.js"></script>
+<script src="assets/frontend/default/js/tinymce.min.js"></script>
+<script src="assets/frontend/default/js/multi-step-modal.js"></script>
+<script src="assets/frontend/default/js/jquery.webui-popover.min.js"></script>
+<script src="https://content.jwplatform.com/libraries/O7BMTay5.js"></script>
+<script src="assets/frontend/default/js/main.js"></script>
+<script src="assets/global/toastr/toastr.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/nestable2/1.6.0/jquery.nestable.min.js" charset="utf-8"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js" integrity="sha384-FzT3vTVGXqf7wRfy8k4BiyzvbNfeYjK+frTVqZeNDFl8woCbF0CYG6g2fMEFFo/i" crossorigin="anonymous"></script>
+<script src="assets/frontend/default/js/bootstrap-tagsinput.min.js"></script>
+<script src="assets/frontend/default/js/custom.js"></script>
+
 
 
 <section class="category-course-list-area">
